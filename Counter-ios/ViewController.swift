e0550c98a0c8611c4bf87682d16d8ece3d6b7c51
Aleksei Kolesnikov <ancientt1k1@gmail.com>
2023-04-17 23:02:42 +0300

    Выполнена усложнённая задача. Добавлены кнопки уменьшения и очищения счетчика. У кнопки увеличения счетчика добавлена иконка. Добавлено текстовое поле для отображения истории нажатия кнопок с датой.

--- Counter-ios/ViewController.swift
+++ Counter-ios/ViewController.swift
@@ -9,18 +9,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Лейбл
     @IBOutlet weak var counterLabel: UILabel!
+    
+    //Кнопки
     @IBOutlet weak var increaseButton: UIButton!
+    @IBOutlet weak var decreaseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
     
-    var count: Int = 0
+    //Text view
+    @IBOutlet weak var historyView: UITextView!
     
-    func updateCounterLabel() {
+    //Счетчик
+    private var count: Int = 0
+
+    //Перечисление действий кнопок
+    private enum Actions {
+        case increase
+        case decrese
+        case decreseZero
+        case reset
+    }
+    
+    private var action: Actions = .reset
+    
+    //функция возвращает текущую дату в красивом формате
+    private func getCurrentDate() -> String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d.MM.yyyy HH:mm:ss"
+        
+        return dateFormatter.string(from: date)
+    }
+    
+    //Функция обновляет text view в зависмости от действия
+    private func updateTextView(buttonAction: Actions) {
+        let date = getCurrentDate()
+        
+        switch buttonAction {
+            case .decrese:
+                historyView.text += "\n[\(date)]: значение изменено на -1"
+            case .decreseZero:
+                historyView.text += "\n[\(date)]: попытка уменьшить значение счётчика ниже 0"
+            case .increase:
+                historyView.text += "\n[\(date)]: значение изменено на +1"
+            case .reset:
+                historyView.text += "\n[\(date)]: значение сброшено"
+        }
+    }
+    
+    //Функция обновляет счетчик
+    private func updateCounterLabel() {
         counterLabel.text = "Значение счётчика: " + String(count)
     }
     
     @IBAction func increseByOne() {
         count += 1
+        action = .increase
+        
         updateCounterLabel()
+        updateTextView(buttonAction: action)
+    }
+    
+    @IBAction func decreaseByOne() {
+        if count > 0 {
+            count -= 1
+            action = .decrese
+            
+            updateCounterLabel()
+        } else {
+            action = .decreseZero
+        }
+        updateTextView(buttonAction: action)
+    }
+    
+    @IBAction func resetCounter() {
+        count = 0
+        action = .reset
+        
+        updateCounterLabel()
+        updateTextView(buttonAction: action)
     }
     
     override func viewDidLoad() {
